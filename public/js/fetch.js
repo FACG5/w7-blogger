233999dfcb3208c949307dce0be8498b6546b22b
ali <ali.7@outlook.com>
2018-08-14 14:52:45 +0300

    add-file-structue

---
+++ public/js/fetch.js
@@ -0,0 +1,14 @@
+const fetch = (data, method, url, cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        cb(null, xhr.responseText);
+      } else {
+        cb(xhr.responseType);
+      }
+    }
+  };
+  xhr.open(method, url);
+  xhr.send(data);
+};
