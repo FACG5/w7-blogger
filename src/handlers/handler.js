7cd1aaf36e3cf55130b6c82c52a7baaee7d18cb5
AsalaKM <asalamatarya@gmail.com>
2018-08-15 10:11:25 +0300

    edit router and add static files

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -1,13 +1,13 @@
 const {
   handleHomePage, handlePageNotFound, signupPage, loginPage,
 } = require('./homePage.js');
-
+const handleStaticFiles = require('./handleStaticFiles.js');
 
 module.exports = {
 
   handleHomePage,
   handlePageNotFound,
-
+  handleStaticFiles,
   signupPage,
   loginPage,
 
